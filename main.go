174fcfb5f1c4d0f42143c26f9f765ea1969f0410
Dmitry Shurshilin <dmitry.shurshilin@gmail.com>
2023-02-08 09:20:06 +0400

    Add one more public test method

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package test
 
+import (
+	"strconv"
+)
+
 func GetSomeTestValueFromModule() string {
 	return "Some Test Value From Module"
+}
+
+func ConvertIntToString(value int) string {
+	return strconv.Itoa(value)
 }
